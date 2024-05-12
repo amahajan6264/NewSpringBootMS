ef3e93837f2c048fc12fb57ab4fe1700273decc1
amahajan6264 <amahajan6264@gmail.com>
2024-05-12 16:00:55 +0530

    data source

--- BillingProducerApplication/src/main/java/com/ak/BillingProducerApplication.java
+++ BillingProducerApplication/src/main/java/com/ak/BillingProducerApplication.java
@@ -25,7 +25,7 @@ public class BillingProducerApplication {
 
 	//@Bean
     public DataSource dataSource() {
-	    System.out.println("Data source creattion")
+	    System.out.println("Data source creattion");
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(dirver);
         dataSource.setUrl(url);
