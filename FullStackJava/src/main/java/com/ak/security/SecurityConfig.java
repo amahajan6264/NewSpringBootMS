8c9573f3880151cdbb28814b4f28f594c25f4dbe
amahajan6264 <amahajan6264@gmail.com>
2024-06-19 23:23:56 +0530

    merge

--- FullStackJava/src/main/java/com/ak/security/SecurityConfig.java
+++ FullStackJava/src/main/java/com/ak/security/SecurityConfig.java
@@ -26,7 +26,6 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		System.out.println("SecurityConfig.securityFilterChain()");
 		
 		List<UrlAccess> urlAccessList = urlAccessRepository.findAll();
 
